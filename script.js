7dc299aa119eed3983fdd74645a1fc202df56809
Noman Rafiq <heyimnomee@gmail.com>
2024-11-06 19:52:39 +0500

    working

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true,
+const elements = document.querySelectorAll('.element');
+const fixedImage = document.querySelector('#fixed-image');
+elements.forEach((element) => {
+    element.addEventListener('mouseenter', (e) => {
+        if (e.target.className === 'element') {
+            const image = e.target.getAttribute('data-image');
+            fixedImage.style.display = 'block';
+            fixedImage.style.backgroundImage = `url(${image})`;
+        }
+    });
+    element.addEventListener('mouseleave', (e) => {
+        if (e.target.className === 'element') {
+            fixedImage.style.display = 'none';
+        }
+    });
 });
 
-console.log(scroll);
+var swiper = new Swiper('.mySwiper', {
+    cssMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    mousewheel: true,
+    keyboard: true,
+});
